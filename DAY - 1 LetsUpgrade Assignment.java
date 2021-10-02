f7318c4ef8901b2301d9bed775e92d8ae38e7a6c
rubinofficial-78 <91709659+rubinofficial-78@users.noreply.github.com>
2021-10-02 07:47:11 +0530

    Java - Zero to Hero Assignment Submitted

---
+++ DAY - 1 LetsUpgrade Assignment.java
@@ -0,0 +1,10 @@
+//Java Program to print my Introduction in the Screen
+class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is RUBIN KUMAR K");
+        System.out.println(
+                "I'm currrently persuing my UG II-nd Year in Computer Science and Engineering in Sri Eshwar college of Engineering");
+        System.out.println("I'm from Udumalpet,Tamil Nadu");
+        System.out.println("Let's learn with LetsUpgrade and Let's get Upgraded :)");
+    }
+}
